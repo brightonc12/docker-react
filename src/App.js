fef5ef4cb1a4314eb71ff053af6732ff8d5b8c3c
brighton <brighton.chabula@zambezii.com>
2019-07-31 11:33:08 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hi there My name is Brighton
         </a>
       </header>
     </div>
